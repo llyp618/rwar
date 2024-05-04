432630890093a4a2cc19f4e68303943687c30158
llyp618 <llyp618@163.com>
2024-05-04 20:05:06 +0800

    init

--- a/dist/src/minus.d.ts
+++
@@ -1,3 +0,0 @@
-type Minus = (x: number, y: number) => number;
-export declare const minus: Minus;
-export {};
--- b/dist/src/plus.d.ts
+++
@@ -1,3 +0,0 @@
-type Plus = (x: number, y: number) => number;
-export declare const plus: Plus;
-export {};
